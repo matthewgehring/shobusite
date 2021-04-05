0c1867381055c3cfd9f341b28e01197f8d3ef3f5
unknown <mgehring9@gmail.com>
2021-04-05 11:16:09 -0700

    handle server error

--- sessionObject.js
+++ sessionObject.js
@@ -73,8 +73,12 @@ class Session{
         python.stdout.on('data', (data) => {
         console.log('Pipe data from python script ...');
         dataToSend = data.toString();
-        dataToSend = dataToSend.replace('\r\n', '')
-        this.updateBoard(dataToSend.split('$'));
+        dataToSend = dataToSend.replace(/(\r\n|\r|\n)/, '')
+        if(!dataToSend.includes("Error")){
+            this.updateBoard(dataToSend.split('$'));
+        } else {
+            this.Broadcast("update",this.gameState);
+        }
         });
         // in close event we are sure that stream from child process is closed
         python.on('close', (code) => {
