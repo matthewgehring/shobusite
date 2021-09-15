874003d5f474ae26369a83a259c38a25a6c6359b
unknown <mgehring9@gmail.com>
2021-09-15 09:13:26 -0700

    add pulsing board logic

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -2,9 +2,16 @@ import React from 'react';
 import Square from './Square';
 
 const Board = (props) => {
+    const [turn, setTurn] = React.useState(props.isPlayer_one)
+
+    React.useEffect(() => {
+        socket.on("player-turn", (data) => {
+            setTurn(data)
+        })
+    })
     
     return (
-        <div className= {"board" + ((props.gameState.p1_turn & props.pulse[props.region]) ? " region0" : "")}>
+        <div className= {"board" + ((turn & props.pulse[props.region]) ? " region0" : "")}>
             {props.gameState.grids[props.region].map((value,index) => {
                 return <Square region={props.region} val={value.toString()} key={index} index={index} isPlayer_one={props.isPlayer_one} gameState={props.gameState}/>
             }) }
